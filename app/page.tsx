3694b67ab6cace8a6cb171df2fccabc88fcd93a4
tak40548798 <tak40548798@gmail.com>
2024-01-24 02:23:03 +0800

    完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
-import { ProductSlides } from "@/components/product/ProductSlides";
-import { PC_PRODUCT_LIST } from "@/constants";
+import Product from "./Product";
 
 export default function Home() {
   return (
-    <main>
-      <div className="mx-auto flex max-w-[90%] flex-col items-center min-[1920px]:max-w-[1640px]">
+    <main className="flex items-center h-screen">
+      <div className="mx-auto flex max-w-[90%] flex-col items-center min-[1920px]:max-w-[1640px] max-h-full overflow-y-visible">
         <div className="hidden text-center font-bold md:block">
           <div className="text-[46px] leading-[51px]">
             Best Seller Gaming PC
           </div>
           <div className="text-[30px] leading-[33px]">Prebuilt & Custom</div>
         </div>
-        <ProductSlides list={PC_PRODUCT_LIST} />
-        {/* <ProductSlides list={PC_PRODUCT_LIST.slice(0,1)} /> */}
-        {/* <ProductSlides list={PC_PRODUCT_LIST.slice(0,3)} /> */}
+        <Product />
       </div>
     </main>
   );
